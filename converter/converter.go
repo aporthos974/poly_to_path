a24bce498ff4c01928b396e3c4ad08768893694b
Anthony Porthos <anthony.porthos@orange.com>
2015-02-13 21:33:59 +0100

    convert poly to path

--- converter/converter.go
+++ converter/converter.go
@@ -4,6 +4,7 @@ import (
 	"github.com/moovweb/gokogiri"
 	"github.com/moovweb/gokogiri/xpath"
 	"log"
+	"strings"
 )
 
 func Convert(svg string) string {
@@ -13,13 +14,25 @@ func Convert(svg string) string {
 	}
 	defer document.Free()
 
-	expression := xpath.Compile("//*[local-name() = 'polygon']")
-	polygons, err := document.Search(expression)
+	expression := xpath.Compile("//*[local-name() = 'polygon' or local-name() = 'polyline']")
+	tags, err := document.Search(expression)
 	if err != nil {
 		log.Fatalf("Unable to find path : %s", err.Error())
 	}
-	for _, polygon := range polygons {
-		polygon.SetName("path")
+	for _, tag := range tags {
+		dAttribut := tag.Attribute("points")
+		dAttribut.SetName("d")
+
+		points := "M"
+		coordonates := strings.SplitN(dAttribut.Value(), " ", 2)
+		points = points + coordonates[0] + " L" + coordonates[1]
+
+		if tag.Name() == "polygon" {
+			points = points + "Z"
+		}
+
+		dAttribut.SetValue(points)
+		tag.SetName("path")
 	}
 	return document.InnerHtml()
 }
--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"polygon_to_path/converter"
+)
 
+func main() {
+	content, _ := ioutil.ReadFile("reunion.svg")
+	fmt.Printf(converter.Convert(string(content)))
 }
